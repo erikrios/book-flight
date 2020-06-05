4571fefad8a5ffa79050af0b1e3696bb7eaa924a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-05 21:15:09 +0700

    Add flight destination

--- lib/home.dart
+++ lib/home.dart
@@ -6,14 +6,62 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        padding: EdgeInsets.all(16.0),
         alignment: Alignment.center,
         color: Colors.blue,
-        child: Column(
-          children: [
-            
-          ],
+        child: Container(
+          margin: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              FlightDestination('Indonesia Airways', 'Surabaya to Jakarta'),
+              Container(
+                margin: EdgeInsets.only(top: 16.0),
+              ),
+              FlightDestination('Air Asia', 'Jakarta to Silicon Valley'),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+class FlightDestination extends StatelessWidget {
+  final String plane;
+  final String destination;
+
+  FlightDestination(this.plane, this.destination, {key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        children: [
+          Expanded(
+            child: Text(
+              plane,
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Source Sans Pro',
+                fontWeight: FontWeight.w700,
+                fontSize: 30.0,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              destination,
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Architects Daughter',
+                fontSize: 20.0,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ),
+        ],
+      );
 }
